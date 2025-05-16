ea1a0cbc9257039449973b5228aafa874002987d
mikhnenko <mikhnenko@yandex-team.com>
2025-05-17 02:17:07 +0300

    use sudo

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -8,7 +8,6 @@ sudo apt-get install boinc-client boinc-manager
 sudo systemctl start boinc-client
 sudo usermod -a -G boinc $USER
 sudo systemctl restart boinc-client
-sudo usermod -a -G boinc $USER
 
 URL=http://89.169.171.76/myboinc
 EMAIL=$(hostname)@mail.com
@@ -16,6 +15,6 @@ PASSWORD=password
 NAME=$(hostname)
 
 # sudo systemctl status boinc-client
-boinccmd --create_account $URL ${EMAIL} ${PASSWORD} ${NAME}
-ACCOUNT_KEY=$(boinccmd --lookup_account ${URL} ${EMAIL} ${PASSWORD} ${NAME})
-boinccmd --project_attach ${URL} ${ACCOUNT_KEY}
+sudo boinccmd --create_account $URL ${EMAIL} ${PASSWORD} ${NAME}
+ACCOUNT_KEY=$(sudo boinccmd --lookup_account ${URL} ${EMAIL} ${PASSWORD} ${NAME})
+sudo boinccmd --project_attach ${URL} ${ACCOUNT_KEY}
