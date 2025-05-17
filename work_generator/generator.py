3c6ba9913f61b69230472b3a562529551cdc1455
mikhnenko <mikhnenko@yandex-team.com>
2025-05-18 00:26:53 +0300

    fix

--- work_generator/generator.py
+++ work_generator/generator.py
@@ -6,10 +6,17 @@ import time
 
 TASKS_NUMBER = 50
 SQUARE_SIZE = 10
+BOINC_ROOT = '/home/boincadm/projects/myboinc'
 
 
 def formatName(i):
-    return f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}'
+    return f'latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}'
+
+def getText(square):
+    ans = f'{SQUARE_SIZE}\n'
+    for row in square:
+        ans += ' '.join(row) + '\n'
+    return ans
 
 with open(f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}') as f:
     data = f.read().split('\n')
@@ -22,17 +29,21 @@ for i in range(TASKS_NUMBER):
     if len(square) == SQUARE_SIZE**2:
         a = [square[i*SQUARE_SIZE:i*SQUARE_SIZE+SQUARE_SIZE] for i in range(SQUARE_SIZE)]
 
-        f = os.path.abspath(formatName(i))
+        f = os.path.abspath(os.path.join('../data/', formatName(i)))
         with open(f, 'w') as output:
-            output.write(f'{SQUARE_SIZE}\n')
-            for row in a:
-                output.write(' '.join(row) + '\n')
+            output.write(getText(a))
+        f = os.path.join(BOINC_ROOT, formatName(i))
+        with open(f, 'w') as output:
+            output.write(getText(a))
 
 cwd = os.getcwd()
-os.chdir('/home/boincadm/projects/myboinc')
+os.chdir()
 
 for name in ['Transversal', 'DTransversal']:
-        files = [os.path.join(cwd, formatName(i)) for i in range(TASKS_NUMBER)]
+        files = [formatName(i) for i in range(TASKS_NUMBER)]
         result = subprocess.check_output(['bin/create_work', '--appname', name] + files).decode()
         print(result)
         time.sleep(1)
+
+for i in range(TASKS_NUMBER):
+    os.remove(formatName(i))
