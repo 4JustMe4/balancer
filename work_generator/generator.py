b247de9edcf01713e3a507c6237ac8c7588141f5
mikhnenko <mikhnenko@yandex-team.com>
2025-05-18 00:46:39 +0300

    create different files for different tasks

--- work_generator/generator.py
+++ work_generator/generator.py
@@ -8,8 +8,8 @@ TASKS_NUMBER = 100
 SQUARE_SIZE = 10
 
 
-def formatName(i):
-    return f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}'
+def formatName(i, suff):
+    return f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}_{suff}'
 
 with open(f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}') as f:
     data = f.read().split('\n')
@@ -22,17 +22,18 @@ for i in range(TASKS_NUMBER):
     if len(square) == SQUARE_SIZE**2:
         a = [square[i*SQUARE_SIZE:i*SQUARE_SIZE+SQUARE_SIZE] for i in range(SQUARE_SIZE)]
 
-        f = os.path.abspath(formatName(i))
-        with open(f, 'w') as output:
-            output.write(f'{SQUARE_SIZE}\n')
-            for row in a:
-                output.write(' '.join(row) + '\n')
+        for suff in ['Transversal', 'DTransversal']:
+            f = os.path.abspath(formatName(i, suff))
+            with open(f, 'w') as output:
+                output.write(f'{SQUARE_SIZE}\n')
+                for row in a:
+                    output.write(' '.join(row) + '\n')
 
 cwd = os.getcwd()
 os.chdir('/home/boincadm/projects/myboinc')
 
 for name in ['Transversal', 'DTransversal']:
     for i in range(TASKS_NUMBER):
-        result = subprocess.check_output(['bin/submit_job', name, os.path.join(cwd, formatName(i))]).decode()
+        result = subprocess.check_output(['bin/submit_job', name, os.path.join(cwd, formatName(i, suff))]).decode()
         print(result)
         time.sleep(1)
