0b5b74de7513b8cd9356ead9b6a0690e4816a6cd
mikhnenko <mikhnenko@yandex-team.com>
2025-05-18 00:36:25 +0300

    stage files before

--- work_generator/generator.py
+++ work_generator/generator.py
@@ -18,7 +18,10 @@ def getText(square):
         ans += ' '.join(row) + '\n'
     return ans
 
-with open(f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}') as f:
+cwd = os.getcwd()
+os.chdir(BOINC_ROOT)
+
+with open(os.path.join(cwd, f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}')) as f:
     data = f.read().split('\n')
 
 # random.shuffle(data)
@@ -29,21 +32,13 @@ for i in range(TASKS_NUMBER):
     if len(square) == SQUARE_SIZE**2:
         a = [square[i*SQUARE_SIZE:i*SQUARE_SIZE+SQUARE_SIZE] for i in range(SQUARE_SIZE)]
 
-        f = os.path.abspath(os.path.join('../data/', formatName(i)))
+        f = os.path.abspath(os.path.join(cwd, '../data/', formatName(i)))
         with open(f, 'w') as output:
             output.write(getText(a))
-        f = os.path.join(BOINC_ROOT, formatName(i))
-        with open(f, 'w') as output:
-            output.write(getText(a))
-
-cwd = os.getcwd()
-os.chdir(BOINC_ROOT)
+        result = subprocess.check_output(['bin/stage_file', '--copy', f]).decode()
 
 for name in ['Transversal', 'DTransversal']:
         files = [formatName(i) for i in range(TASKS_NUMBER)]
         result = subprocess.check_output(['bin/create_work', '--appname', name] + files).decode()
         print(result)
         time.sleep(1)
-
-for i in range(TASKS_NUMBER):
-    os.remove(formatName(i))
