c5503fe93d014b4e2efbd2f4cdab79a90a54e530
mikhnenko <mikhnenko@yandex-team.com>
2025-05-17 18:21:12 +0300

    submit jobs

--- work_generator/dtransversal.py
+++ work_generator/dtransversal.py
@@ -1,20 +1,32 @@
-import random
 import os
+import random
+import subprocess
+
 
 TASKS_NUMBER = 100
 SQUARE_SIZE = 10
+
+
+def formatName(i):
+    return f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}'
 
 with open(f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}') as f:
     data = f.read().split('\n')
 
 random.shuffle(data)
-for i in range(100):
+for i in range(TASKS_NUMBER):
+    if i % 10 == 0:
+        print(f'create task number {i}')
     square = data[i]
     if len(square) == SQUARE_SIZE**2:
         a = [square[i*SQUARE_SIZE:i*SQUARE_SIZE+SQUARE_SIZE] for i in range(SQUARE_SIZE)]
 
-        f = os.path.abspath(f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}')
+        f = os.path.abspath(formatName(i))
         with open(f, 'w') as output:
             output.write(f'{SQUARE_SIZE}\n')
             for row in a:
                 output.write(' '.join(row) + '\n')
+
+for i in range(TASKS_NUMBER):
+    result = subprocess.check_output(['/home/boincadm/projects/myboinc/bin/submit_job', 'DTransversal', formatName(i)])
+    print(result)
