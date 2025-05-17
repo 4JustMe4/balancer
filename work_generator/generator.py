d2671a989d2bac39cc13ac54eda0303d93b26a0f
mikhnenko <mikhnenko@yandex-team.com>
2025-05-18 00:40:21 +0300

    use submit_job

--- work_generator/generator.py
+++ work_generator/generator.py
@@ -4,24 +4,14 @@ import subprocess
 import time
 
 
-TASKS_NUMBER = 50
+TASKS_NUMBER = 100
 SQUARE_SIZE = 10
-BOINC_ROOT = '/home/boincadm/projects/myboinc'
 
 
 def formatName(i):
-    return f'latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}'
+    return f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}_task_{i}'
 
-def getText(square):
-    ans = f'{SQUARE_SIZE}\n'
-    for row in square:
-        ans += ' '.join(row) + '\n'
-    return ans
-
-cwd = os.getcwd()
-os.chdir(BOINC_ROOT)
-
-with open(os.path.join(cwd, f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}')) as f:
+with open(f'../data/latin{SQUARE_SIZE}x{SQUARE_SIZE}') as f:
     data = f.read().split('\n')
 
 # random.shuffle(data)
@@ -32,13 +22,17 @@ for i in range(TASKS_NUMBER):
     if len(square) == SQUARE_SIZE**2:
         a = [square[i*SQUARE_SIZE:i*SQUARE_SIZE+SQUARE_SIZE] for i in range(SQUARE_SIZE)]
 
-        f = os.path.abspath(os.path.join(cwd, '../data/', formatName(i)))
+        f = os.path.abspath(formatName(i))
         with open(f, 'w') as output:
-            output.write(getText(a))
-        result = subprocess.check_output(['bin/stage_file', '--copy', f]).decode()
+            output.write(f'{SQUARE_SIZE}\n')
+            for row in a:
+                output.write(' '.join(row) + '\n')
+
+cwd = os.getcwd()
+os.chdir('/home/boincadm/projects/myboinc')
 
 for name in ['Transversal', 'DTransversal']:
-        files = [formatName(i) for i in range(TASKS_NUMBER)]
-        result = subprocess.check_output(['bin/create_work', '--appname', name] + files).decode()
+    for i in range(TASKS_NUMBER):
+        result = subprocess.check_output(['bin/submit_job', name, os.path.join(cwd, formatName(i))]).decode()
         print(result)
         time.sleep(1)
