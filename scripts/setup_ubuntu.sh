564870c45ad742c961943ce9650f50e620e38a1a
mikhnenko <mikhnenko@yandex-team.com>
2025-05-23 01:51:25 +0300

    add new hosts

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -3,18 +3,18 @@
 set -xue
 
 sudo apt-get update
-sudo apt-get install boinc-client boinc-manager
+echo 'Y' | sudo apt-get install boinc-client boinc-manager
 
 sudo systemctl start boinc-client
 sudo usermod -a -G boinc $USER
 sudo systemctl restart boinc-client
 
-URL=http://89.169.171.76/myboinc
-EMAIL=$(hostname)@mail.com
-PASSWORD=password
-NAME=$(hostname)
+export URL=http://89.169.171.76/myboinc
+export EMAIL=$(hostname)-retry@mail.com
+export PASSWORD=password
+export NAME=$(hostname)-retry
 
-# sudo systemctl status boinc-client
-sudo boinccmd --create_account $URL ${EMAIL} ${PASSWORD} ${NAME}
-ACCOUNT_KEY=$(sudo boinccmd --lookup_account ${URL} ${EMAIL} ${PASSWORD} ${NAME})
+# waiting for local boinc
+sudo boinccmd --create_account ${URL} ${EMAIL} ${PASSWORD} ${NAME} || sleep 10 && sudo boinccmd --create_account ${URL} ${EMAIL} ${PASSWORD} ${NAME}
+export ACCOUNT_KEY=$(boinccmd --lookup_account ${URL} ${EMAIL} ${PASSWORD} ${NAME} | grep -oP 'account key: \K.*')
 sudo boinccmd --project_attach ${URL} ${ACCOUNT_KEY}
