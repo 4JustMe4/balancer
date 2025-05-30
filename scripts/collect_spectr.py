c79e8723bf0ce487b7fe938f6c24be19e4e70ff5
mikhnenko <mikhnenko@yandex-team.com>
2025-05-31 00:45:32 +0300

    better scripts

--- scripts/collect_spectr.py
+++ scripts/collect_spectr.py
@@ -1,36 +1,42 @@
 import os
 import re
 
-BASE_DIR = os.path.expanduser('~/projects/myboinc/results/0')
+PROJECT_DIR = os.path.expanduser('~/projects/myboinc/')
+
+def produce(regex):
+    found_numbers = set()
+    number_to_filename = dict()
+    value_to_number = dict()
+
+    for results_dir in [os.path.join(PROJECT_DIR, 'result'), os.path.join(PROJECT_DIR, 'upload')]
+        for root, dirs, files in os.walk(results_dir):
+            for fname in files:
+                match = regex.match(fname)
+                if match:
+                    number = int(match.group(1))
+                    found_numbers.add(number)
+                    full_path = os.path.join(root, fname)
+
+                    try:
+                        with open(full_path, 'r') as f:
+                            content = f.read().strip()
+                            first_line = next(filter(None, content.splitlines()))
+                            val = int(first_line)
+                            value_to_number.setdefault(val, full_path)
+                    except Exception as e:
+                        print(f"Can't read file {full_path}: {e}")
+
+    expected_numbers = set(range(0, 2001))
+    missing_numbers = sorted(expected_numbers - found_numbers)
+
+    print("Missed squares:", missing_numbers)
+
+    print("Dict (value -> square_number):")
+    for val, number in value_to_number.items():
+        print(f"{val} -> {number}")
 
 transversal_re = re.compile(r'^Transversal_(\d{1,5})_')
+dtransversal_re = re.compile(r'^DTransversal_(\d{1,5})_')
 
-found_numbers = set()
-number_to_filename = dict()
-value_to_number = dict()
-
-for root, dirs, files in os.walk(BASE_DIR):
-    for fname in files:
-        match = transversal_re.match(fname)
-        if match:
-            number = int(match.group(1))
-            found_numbers.add(number)
-            full_path = os.path.join(root, fname)
-            
-            try:
-                with open(full_path, 'r') as f:
-                    content = f.read().strip()
-                    first_line = next(filter(None, content.splitlines()))
-                    val = int(first_line)
-                    value_to_number.setdefault(val, full_path)
-            except Exception as e:
-                print(f"Can't read file {full_path}: {e}")
-
-expected_numbers = set(range(0, 2001))
-missing_numbers = sorted(expected_numbers - found_numbers)
-
-print("Missed squares:", missing_numbers)
-
-print("Dict (value -> square_number):")
-for val, number in value_to_number.items():
-    print(f"{val} -> {number}")
+produce(transversal_re)
+produce(dtransversal_re)
